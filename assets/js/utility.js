ebc47abf1786598e1c3e28d440b00513333cd413
Farisul <farisulalamruddro@gmail.com>
2024-02-19 00:11:25 +0600

    Functions Done

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,6 +1,6 @@
 // Global Variables
 
-let pricePerSeat = 500;
+let pricePerSeat = 550;
 let totalSeats = 40;
 let totalSelectedSeats = 0;
 
@@ -15,25 +15,80 @@ function seatPlans() {
 }
 
 function handleCheckbox(seat) {
-    let isChecked = seat.target.checked;
-    let inputLabel = seat.target.labels[0].classList;
+  var maxAllowed = 4;
+  let isChecked = seat.target.checked;
+  let label = seat.target.name;
+  let inputLabel = seat.target.labels[0].classList;
 
+  // Use checkedCheckboxes.length instead of isChecked.length
+  let checkedCheckboxes = document.querySelectorAll('#seatPlan input[type="checkbox"]:checked');
+
+  if (checkedCheckboxes.length > maxAllowed) {
+    alert("You can only select up to " + maxAllowed + " seats.");
+    this.checked = false; // Uncheck the last checked checkbox
+  } else {
     seatActive(isChecked, inputLabel);
     seatsSelected(isChecked);
+    checkoutItem(isChecked, label);
+  }
 }
+
 
 
 // Changing color on seat active
-
 function seatActive(checked, label) {
   if (checked) {
-    label.replace("bg-ash", "bg-green");
-    label.replace("text-dark/50", "text-white");
+    label.remove("bg-ash");
+    label.add("bg-green");
+    label.remove("text-dark/50");
+    label.add("text-white");
   } else {
-    label.replace("bg-green", "bg-ash");
-    label.replace("text-white", "text-dark/50");
+    label.remove("bg-green");
+    label.add("bg-ash");
+    label.remove("text-white");
+    label.add("text-dark/50");
   }
 }
+
+
+// Adding to checkout
+
+function checkoutItem(checked, label) {
+  let itemsContainer = document.getElementById("seatsCount");
+
+  if (checked) {
+    let itemRow = document.createElement("div");
+    itemRow.classList.add("grid", "grid-cols-5", "gap-2");
+
+    // Add content to the created div (you can customize this part)
+    itemRow.innerHTML = `
+      <div class="col-span-2">
+                                            <p>${label}</p>
+                                        </div>
+                                        <div class="col-span-2">
+                                            <p>Economoy</p>
+                                        </div>
+                                        <div class="col-span-1 text-end">
+                                            <p>550</p>
+                                        </div>
+    `;
+
+    itemRow.setAttribute("data-checkbox-id", label);
+
+    itemsContainer.appendChild(itemRow);
+  } else {
+    // If the checkbox is not checked, find and remove the corresponding div
+    let divToRemove = itemsContainer.querySelector(
+      `[data-checkbox-id="${label}"]`
+    );
+    if (divToRemove) {
+      itemsContainer.removeChild(divToRemove);
+    }
+  }
+}
+
+
+
 
 // Total Selected Seats
 
@@ -50,6 +105,7 @@ function seatsSelected(e) {
     availableSeats(totalSelectedSeats);
     totalPrice(totalSelectedSeats);
 }
+
 
 // Available Seats
 
